f025fa26f097d923981bab577af5030bfc6d91ca
Simon Lynch <simon.lynch@hashicorp.com>
2025-05-15 13:45:07 +1000

    Add HCP group resource and output for group ID in Terraform configuration

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "hcp_project" "this" {
   name        = var.hcp_project_name
   description = var.hcp_project_description
 }
+
+resource "hcp_group" "this" {
+  display_name = var.hcp_project_name
+  description  = "${var.hcp_project_description} group"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,7 @@
 output "hcp_project_name" {
   value = hcp_project.this.name
-  
+}
+
+output "hcp_group" {
+  value = hcp_group.this.id
 }
