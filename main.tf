7406923caac5c2a4fa5c1cfb2dae99ed1bf73382
Simon Lynch <simon.lynch@hashicorp.com>
2025-05-15 13:50:14 +1000

    Add IAM policy data for HCP group to attach contributor role

--- main.tf
+++ main.tf
@@ -3,7 +3,26 @@ resource "hcp_project" "this" {
   description = var.hcp_project_description
 }
 
+# Create a group in HCP
 resource "hcp_group" "this" {
   display_name = var.hcp_project_name
   description  = "${var.hcp_project_description} group"
+}
+
+
+data "hcp_iam_policy" "project" {
+  bindings = [
+    {
+      role = "roles/contributor"
+      principals = [
+        "${hcp_group.this.group_id}",
+      ]
+    },
+  ]
+}
+
+# Attach the IAM policy to the project
+resource "hcp_project_iam_policy" "project_policy" {
+  project_id  = hcp_project.this.resource_id
+  policy_data = data.hcp_iam_policy.project.policy_data
 }
